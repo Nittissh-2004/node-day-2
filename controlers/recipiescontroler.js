0a2bcbd7411f30a28e304b2da99ca0af7e61988f
Nittissh-2004 <nittissh2004@gmail.com>
2025-05-22 14:25:49 +0530

    completed

---
+++ controlers/recipiescontroler.js
@@ -0,0 +1,89 @@
+import recipes from "../Models/recipeSchema.js";
+
+//create product by using Post method
+
+export const createRecipe = async (req, res) => {
+  try {
+    //req.body will get data from frontend that asigning in single sline
+    const newRecipe = new recipes(req.body);
+    //tosave new details in mongoDB
+    await newRecipe.save();
+    res
+      .status(200)
+      .json({ message: "Recipe Created Successfully", data: newRecipe });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//get all Recipes
+
+export const getAllRecipes = async (req, res) => {
+  try {
+    const getRecipes = await recipes.find();
+    res
+      .status(200)
+      .json({ message: "Recipes Retrived Successfully", data: getRecipes });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//get Recipe By Id
+
+export const getRecipeById = async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const recipeDetails = await recipes.findById(recipeId);
+    if (!recipeDetails) {
+      return res.status(404).json({ message: "Recipe Not Found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Recipe Retrived Successfully", data: recipeDetails });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//update Recipe function
+
+export const updateRecipe = async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const { recipename , formula , ingredients } = req.body
+
+    const result = await recipes.findByIdAndUpdate(
+      { _id: recipeId },
+      { recipename , formula , ingredients },
+      { recipename, formula, ingredients },
+      { new: true }
+    );
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Recipe Not Found" });
+    }
+    res
+      .status(200)
+      .json({ message: "Recipe Updated Succesfully", data: result });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+//Delete Recipe
+
+export const deleteRecipe = async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+    const result = await recipes.findByIdAndDelete({ _id: recipeId });
+    if (!result) {
+      return res.status(404).json({ message: "Recipe Not Found" });
+    }
+    const recipe = await recipes.find();
+    res
+      .status(200)
+      .json({ message: "Recipe Deleted Successfully", data: recipe });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './Ddbconfig.js';
+import recipeRoute from './Routers/recipeRouter.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./Ddbconfig.js";
+import recipeRoute from "./Routers/recipeRouter.js";
+
+// configure dotenv file
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+//setup default middleware
+app.use(express.json());
+app.use(cors());
+
+// connect DB by calling connectDB Function
+connectDB();
+
+//default router
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Welcom To Backend"})
+})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcom To Backend" });
+});
+//custom router
+
+app.use("/api/recipes",recipeRoute)
+app.use("/api/recipes", recipeRoute);
+
+//port initialization
+const port = process.env.PORT;
+
+// server starting function
+app.listen(port,()=>{
+    console.log("server started");
+    
+})
+app.listen(port, () => {
+  console.log("server started");
+});
